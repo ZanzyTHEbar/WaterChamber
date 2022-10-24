262dfd60d74c0d32c41c7c3ea50efcd9db5080ed
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-10-24 19:45:03 +0100

    update - fix compile error with StateManager

--- WaterChamber/lib/WaterChamber/src/local/data/StateManager/StateManager.cpp
+++ WaterChamber/lib/WaterChamber/src/local/data/StateManager/StateManager.cpp
@@ -1,8 +1,4 @@
 #include "StateManager.hpp"
 
-StateManager<State_e> stateManager;
-StateManager<WiFiState_e> wifiStateManager;
-StateManager<WebServerState_e> webServerStateManager;
-StateManager<MDNSState_e> mdnsStateManager;
-StateManager<PumpState_e> PumpStateManager;
+StateManager<SensorStates_e> SensorStatesManager;
 StateManager<LEDStates_e> ledStateManager;
