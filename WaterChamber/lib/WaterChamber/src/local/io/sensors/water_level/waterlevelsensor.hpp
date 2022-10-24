fda943f3b9c00f2e6f1c7630caba02d5872fdda7
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-10-24 19:51:49 +0100

    update - remove unnecessary code from water level sensor - fix compile error for water level sensor

--- WaterChamber/lib/WaterChamber/src/local/io/sensors/water_level/waterlevelsensor.hpp
+++ WaterChamber/lib/WaterChamber/src/local/io/sensors/water_level/waterlevelsensor.hpp
@@ -8,9 +8,6 @@
 #include <memory>
 #include <functional>
 #include <HCSR04.h>
-#if USE_CAP
-#include "calibrationbutton.hpp"
-#endif // USE_CAP
 
 #include "local/io/sensors/temperature/towertemp.hpp"
 
@@ -20,19 +17,6 @@ private:
     // Private variables
     double _radius;
     double _height;
-    bool _activateCalibration;
-    byte _depth;
-    byte _depthRange;
-    int _depthArray[100];
-    int _qNumberReadings[100];
-    int _qNumberDepth[100];
-    struct Calibration_t
-    {
-        int _min;
-        int _max;
-    };
-    Calibration_t _calibration;
-
     struct Data_t
     {
         int water_level;
@@ -41,39 +25,16 @@ private:
 
     // Private functions
     std::shared_ptr<UltraSonicDistanceSensor> _distanceSensor; // Initialize sensor that uses digital pins 13 and 12.
-#if USE_CAP
-    int readWaterLevelCapacitive();
-    void calibrateSensor();
-    void setCapSensorRange();
-    int convertToQNumber(int readings);
-    void convertToQNumber(int *readings, int *depth);
-
-    void holdCallback(void);
-    void quickCallback(void);
-    void longholdCallback(void);
-
-    CalibrationButton *_calibrationButton;
-#endif // USE_CAP
     TowerTemp *_towerTemp;
 
 public:
     // Constructor
-#if USE_CAP
-    WaterLevelSensor(CalibrationButton *_calibrationButton,
-                     TowerTemp *_towerTemp);
-#else
     WaterLevelSensor(TowerTemp *_towerTemp);
-#endif // USE_CAP
-
     virtual ~WaterLevelSensor();
     void begin();
     double readSensor();
     // Read the water level
-    // int getWaterLevel();
     Data_t readWaterLevelUltraSonic();
-#if USE_CAP
-    int getPercentage();
-#endif
     Data_t result;
 };
 #endif
