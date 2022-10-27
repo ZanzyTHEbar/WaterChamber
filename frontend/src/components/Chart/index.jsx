2f02a734dd4d617a1af2122404dd6e5e8cf56b9b
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-10-27 21:49:05 +0100

    update - finish basic app functionality and look

---
+++ frontend/src/components/Chart/index.jsx
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+//import Highcharts from "highcharts/highmaps";
+import React from "react";
+
+class Chart extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            // To avoid unnecessary update keep all options in the state.
+            chartOptions: {
+                title: { text: this.props.title },
+                accessibility: {
+                    enabled: false,
+                },
+                xAxis: {
+                    type: 'datetime',
+                    dateTimeLabelFormats: { second: '%H:%M:%S' },
+                },
+                yAxis: {
+                    title: { text: this.props.yAxis.title.text },
+                    //title: { text: 'Temperature (Fahrenheit)' }
+                },
+                credits: { enabled: false },
+                series: [{
+                    showInLegend: false,
+                    data: []
+                }],
+                plotOptions: {
+                    line: {
+                        animation: false,
+                        dataLabels: { enabled: true }
+                    },
+                    series: {
+                        color: this.props.line.color,
+                        point: {
+                            events: {
+                                mouseOver: this.setHoverData.bind(this),
+                            },
+                        },
+                    },
+                },
+            },
+            hoverData: null,
+        };
+
+        setInterval(function () {
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    const x = new Date().getTime(),
+                        y = parseFloat(this.responseText);
+                    console.log(this.responseText);
+                    console.log("Testing");
+                    if (this.state.chartOptions.series[0].data.length > 40) {
+                        this.state.chartOptions.series[0].addPoint([x, y], true, true, true);
+                    } else {
+                        this.state.chartOptions.series[0].addPoint([x, y], true, false, true);
+                    }
+                }
+            };
+            xhttp.open("GET", "http://water_chamber.local/api/v1/json/command/data", true);
+            xhttp.send();
+        }, 3000);
+    }
+
+    setHoverData = (e) => {
+        // The chart is not updated because `chartOptions` has not changed.
+        this.setState({ hoverData: e.target.category });
+    };
+
+    updateSeries = () => {
+        // The chart is updated only with new options.
+        this.setState({
+            chartOptions: {
+                series: [{ data: [Math.random() * 5, 2, 1] }],
+            },
+        });
+    };
+
+    render() {
+
+        const { chartOptions, hoverData } = this.state;
+
+        return (
+            <div className="card" >
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    options={chartOptions}
+                    allowChartUpdate={true}
+                    immutable={false}
+                    updateArgs={[true, true, true]}
+                    containerProps={{ className: "chartContainer" }}
+                />
+                <h3>Hovering over {hoverData}</h3>
+                <button onClick={this.updateSeries.bind(this)}>
+                    Update Series
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Chart;
--- frontend/src/pages/FrontEnd/main/index.tsx
+++ frontend/src/pages/FrontEnd/main/index.tsx
@@ -2,13 +2,52 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import Chart from "@components/Chart";
 import Header from "@components/Header";
-
-import Sidebar from "@components/Sidebar";
 import * as React from "react";
 /* import styles from "./index.module.scss"; */
+const chartOptions = {
+    tempChart: {
+        title: "Temperature",
+        data_url: "http://water_chamber.local/api/v1/json/command/data",
+        yAxis: {
+            title: {
+                text: "Temperature (°C)",
+            },
+        },
+        line: {
+            color: "#059e8a",
+        },
+    },
+    humiChart: {
+        title: "Humidity",
+        data_url: "http://water_chamber.local/api/v1/json/command/data",
+        yAxis: {
+            title: {
+                text: "Humidity (%)",
+            },
+        },
+        line: {
+            color: "#18009c",
+        },
+    },
+    humiTempChart: {
+        title: "Humidity Temperature",
+        data_url: "http://water_chamber.local/api/v1/json/command/data",
+        yAxis: {
+            title: {
+                text: "Temperature (°C)",
+            },
+        },
+        line: {
+            color: "#059e8a",
+        },
+    },
+};
 
 export function Main() {
+    const { tempChart, humiChart, humiTempChart } = chartOptions;
+
     const Main = () => (
         <div
             className="h-fit flex flex-col"
@@ -18,12 +57,21 @@ export function Main() {
         >
             <Header />
             <div
-                className="flex flex-1"
+                className="chartContainer"
                 style={{
                     paddingBottom: "20px",
                 }}
             >
-                <Sidebar />
+                <div
+                    className="chart-div"
+                    style={{
+                        overflow: "auto",
+                    }}
+                >
+                    <Chart {...tempChart} />
+                    <Chart {...humiChart} />
+                    <Chart {...humiTempChart} />
+                </div>
             </div>
         </div>
     );
